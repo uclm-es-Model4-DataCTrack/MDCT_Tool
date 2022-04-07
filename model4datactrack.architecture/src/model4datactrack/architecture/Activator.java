642ab5d3448358c86d9310a286c90ced089b0ee0
MiguelMartinez47 <martinezatienzamiguel@gmail.com>
2022-04-07 08:19:34 +0200

    pushing arch src

---
+++ model4datactrack.architecture/src/model4datactrack/architecture/Activator.java
@@ -0,0 +1,8 @@
+package model4datactrack.architecture;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class Activator extends AbstractUIPlugin {
+	public static final String PLUGIN_ID = "model4datactrack.architecture";
+
+}
---
+++ model4datactrack.architecture/src/model4datactrack/architecture/M4DCTModelCreationCommand.java
@@ -0,0 +1,34 @@
+package model4datactrack.architecture;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.papyrus.uml.diagram.common.commands.ModelCreationCommandBase;
+import org.eclipse.papyrus.uml.tools.utils.PackageUtil;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.Profile;
+import org.eclipse.uml2.uml.UMLFactory;
+
+
+public class M4DCTModelCreationCommand extends ModelCreationCommandBase{
+
+
+	@Override
+	protected EObject createRootElement() {
+		return UMLFactory.eINSTANCE.createModel();
+	}
+
+	
+	@Override
+	protected void initializeModel(EObject owner) {
+		super.initializeModel(owner);
+		Package packageOwner = (Package) owner;
+		// Retrieve model4datactrack profile and apply it
+		Profile m4dctProfile = (Profile) PackageUtil.loadPackage(URI.createURI("pathmap://MODEL4DATACTRACK_PROFILE/Model4DataCTrack.profile.uml"), owner.eResource().getResourceSet());
+		if (m4dctProfile != null) {
+			PackageUtil.applyProfile(packageOwner, m4dctProfile, true);
+		}
+	}
+
+	
+}
+
