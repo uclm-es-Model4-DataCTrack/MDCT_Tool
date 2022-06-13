35c4e490c1a8f505f19ab031bf08293e8ec019bf
MiguelMartinez47 <martinezatienzamiguel@gmail.com>
2022-06-13 09:30:19 +0200

    reuploading final project

--- model4datactrack.architecture/src/model4datactrack/architecture/Activator.java
+++
@@ -1,8 +0,0 @@
-package model4datactrack.architecture;
-
-import org.eclipse.ui.plugin.AbstractUIPlugin;
-
-public class Activator extends AbstractUIPlugin {
-	public static final String PLUGIN_ID = "model4datactrack.architecture";
-
-}
--- model4datactrack.architecture/src/model4datactrack/architecture/validationFilter/M4DCTDiagnostician.java
+++
@@ -1,7 +0,0 @@
-package model4datactrack.architecture.validationFilter;
-
-import org.eclipse.papyrus.uml.service.validation.api.UMLDiagnostician;
-
-public class M4DCTDiagnostician extends UMLDiagnostician {
-
-}
